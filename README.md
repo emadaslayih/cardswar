25cd420ee9a97fedd1280690c2a2e69252e94a59
emadaslayih <emad-slayeh@windowslive.com>
2020-06-13 13:24:29 +0300

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# cardswar
-cards war
+# smarthouse
---
+++ precache-manifest.647d4c20311519052ff70d5a8e2e1c11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc41a90a3494917fffd0fafe96489f7",
+    "url": "/cardswar/index.html"
+  },
+  {
+    "revision": "8c002ced21797f4c66bc",
+    "url": "/cardswar/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d8d26efef882680ce8b1",
+    "url": "/cardswar/static/css/main.a026c566.chunk.css"
+  },
+  {
+    "revision": "8c002ced21797f4c66bc",
+    "url": "/cardswar/static/js/2.83619be1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cardswar/static/js/2.83619be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d26efef882680ce8b1",
+    "url": "/cardswar/static/js/main.b1e97c1c.chunk.js"
+  },
+  {
+    "revision": "fbbfde1711ba6c7df1a0",
+    "url": "/cardswar/static/js/runtime-main.bf6cb4d1.js"
+  },
+  {
+    "revision": "84feeb88d9a286b320c5d2cddee139b4",
+    "url": "/cardswar/static/media/Warlogo.84feeb88.png"
+  }
+]);
